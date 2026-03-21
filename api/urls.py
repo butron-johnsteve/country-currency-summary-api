c6e0cff7272034ba366415b43ea8312298268c87
Phil Josh <tongcophiljosh@gmail.com>
2026-03-21 08:15:53 +0800

    Write automated tests for API validation and error respo

--- api/urls.py
+++ api/urls.py
@@ -4,3 +4,5 @@ from .views import CountryCurrencySummaryView
 urlpatterns = [
     path("v1/country-currency-summary/", CountryCurrencySummaryView.as_view(), name="country-currency-summary"),
 ]
+
+#Write automated tests for API validation and error respo
